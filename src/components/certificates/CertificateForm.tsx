d02f2194ef6e16bcf0642770dc7258b9dc75163d
tumusime_david <tumusimedavid14@gmail.com>
2024-09-13 16:27:54 +0200

    Feature/Task-10-KAN-95 Add comment functionality to user context

--- src/components/certificates/CertificateForm.tsx
+++ src/components/certificates/CertificateForm.tsx
@@ -31,6 +31,7 @@ const CertificateForm: React.FC<ICertificateForm> = ({ isEdit, certificateId }: 
   const [error, setError] = useState<string | null>(null);
   const [isSupplierModalOpen, setIsSupplierModalOpen] = useState(false);
   const [isParticipantModalOpen, setIsParticipantModalOpen] = useState(false);
+  const [comments,setComments]=useState<any[]>([])
   const [participants, setParticipants] = useState<{ name: string; department: string; email: string }[]>([]);
   const [openComment,setOpenComment]=useState(false)
   useEffect(() => {
@@ -210,8 +211,13 @@ const CertificateForm: React.FC<ICertificateForm> = ({ isEdit, certificateId }: 
           <div className="comment-container">
               <button type="button" onClick={()=>setOpenComment(true)}>New Comment</button>
           </div>
-          {openComment&&<CommentModal onAddComment={()=>"hello"} onClose={()=>setOpenComment(false)} />}
+          {openComment&&<CommentModal onAddComment={(e)=>{
+            setComments((prev)=>[...prev,e])
+                    }} onClose={()=>setOpenComment(false)} />}
           <div className="participant-group">
+            {comments?.map((c)=>(
+              <><p>{c?.user}</p><p>{c?.text}</p></>
+            ))}
             <div className="participant-container">
               <label>Assigned users</label>
               <button
