fc028e2dc72e05956cf0720cbb79cf121fdbc92d
tumusime_david <tumusimedavid14@gmail.com>
2024-09-30 18:22:41 +0200

    Feature/Task-16: Implemented user APIs functionality - Created UserController to handle user-related endpoints. - Updated Program.cs to register user services and repositories. - Added IUserRepository and UserRepository for database interactions. - Added IUserService and UserService to handle logic.

---
+++ backend/CertificateManagerAPIs/Repositories/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using CertificateManagerAPIs.Data;
+using CertificateManagerAPIs.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CertificateManagerAPIs.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly CertificatedbContext _context;
+        public UserRepository(CertificatedbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUserAsync()
+        {
+            return await _context.Users
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> SearchUsersByNameAsync(string userName)
+        {
+            return await _context.Users
+                .Where(s => s.Name.Contains(userName))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> SearchUsersByFirstNameAsync(string firstName)
+        {
+            return await _context.Users
+                .Where(s => s.Name.Contains(firstName))
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> SearchUsersByUserIdAsync(int userId)
+        {
+            return await _context.Users
+            .Where(s => s.UserId == userId)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<User>> SearchUsersByPlantAsync(string plant)
+        {
+            return await _context.Users
+            .Where(s => s.Plant.Contains(plant))
+                .ToListAsync();
+        }
+    }
+}
---
+++ backend/CertificateManagerAPIs/Services/UserService.cs
@@ -0,0 +1,76 @@
+﻿using CertificateManagerAPIs.DTO;
+using CertificateManagerAPIs.Entities;
+using CertificateManagerAPIs.Repositories;
+
+namespace CertificateManagerAPIs.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<User>> GetAllUserAsync()
+        {
+            return await _userRepository.GetAllUserAsync();
+        }
+
+        public async Task<IEnumerable<UserDto>> SearchUsersByNameAsync(string userName)
+        {
+            var user = await _userRepository.SearchUsersByNameAsync(userName);
+            return user.Select(s => new UserDto
+            {
+                Name = s.Name,
+                FirstName = s.FirstName,
+                UserId = s.UserId,
+                Department = s.Department,
+                Plant = s.Plant,
+                Email = s.Email
+            }).ToList();
+        }
+
+        public async Task<IEnumerable<UserDto>> SearchUsersByFirstNameAsync(string firstName)
+        {
+            var user = await _userRepository.SearchUsersByFirstNameAsync(firstName);
+            return user.Select(s => new UserDto
+            {
+                Name = s.Name,
+                FirstName = s.FirstName,
+                UserId = s.UserId,
+                Department = s.Department,
+                Plant = s.Plant,
+                Email = s.Email
+            }).ToList();
+        }
+
+        public async Task<IEnumerable<UserDto>> SearchUsersByUserIdAsync(int userId)
+        {
+            var user = await _userRepository.SearchUsersByUserIdAsync(userId);
+            return user.Select(s => new UserDto
+            {
+                Name = s.Name,
+                FirstName = s.FirstName,
+                UserId = s.UserId,
+                Department = s.Department,
+                Plant = s.Plant,
+                Email = s.Email
+            }).ToList();
+        }
+
+        public async Task<IEnumerable<UserDto>> SearchUsersByPlantAsync(string plant)
+        {
+            var user = await _userRepository.SearchUsersByPlantAsync(plant);
+            return user.Select(s => new UserDto
+            {
+                Name = s.Name,
+                FirstName = s.FirstName,
+                UserId = s.UserId,
+                Department = s.Department,
+                Plant = s.Plant,
+                Email = s.Email
+            }).ToList();
+        }
+    }
+}
