ffb69901203a0186f4e45533ba9ac8c4059ed2eb
tumusime_david <tumusimedavid14@gmail.com>
2024-09-30 21:42:47 +0200

    Feature/Task-16 Fixing space issues

--- backend/CertificateManagerAPIs/Controllers/UserController.cs
+++ backend/CertificateManagerAPIs/Controllers/UserController.cs
@@ -13,7 +13,8 @@ namespace CertificateManagerAPIs.Controllers
         {
             _userService = userService;
         }
-        [HttpGet("All Participants")]
+
+        [HttpGet("Participants")]
         public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUserAsync()
         {
             var users = await _userService.GetAllUserAsync();
@@ -39,6 +40,7 @@ namespace CertificateManagerAPIs.Controllers
             var user = await _userService.SearchUsersByUserIdAsync(userId);
             return Ok(user);
         }
+
         [HttpGet("Plant")]
         public async Task<IActionResult> SearchUsersByPlantAsync(string plant)
         {
