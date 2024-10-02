087b5a2fec555aa10a6eff6b4899a31e45fc9e73
tumusime_david <tumusimedavid14@gmail.com>
2024-10-02 19:31:17 +0200

    Feature/Task-18 made changes on the database structure to accept pdf file      - Made change on CertificateDto

--- backend/CertificateManagerAPIs/DTO/CertificateUpdateDto.cs
+++ backend/CertificateManagerAPIs/DTO/CertificateUpdateDto.cs
@@ -6,7 +6,7 @@
         public string Type { get; set; } = null!;
         public DateTime ValidFrom { get; set; }
         public DateTime ValidTo { get; set; }
-        public string PdfFile { get; set; } = null!;
+        public byte[] PdfFile { get; set; } = null!;
         public int? UserAssigned { get; set; }
         public List<CommentDto> NewComments { get; set; } = new List<CommentDto>();
         public List<int> AssignedUserIds { get; set; } = new List<int>();
